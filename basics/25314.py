9bef53a087c77dfb9458b2b1b06b773909890790
aszxk012 <greenkmy012@gmail.com>
2023-03-30 12:12:31 +0900

    4days-2

--- basics/25314.py
+++ basics/25314.py
@@ -1,1 +1,13 @@
 # 25314
+# 숫자를 입력받아 정수 자료형의 이름 구하기
+# long int = 4 byte, long long int = 8 byte ...
+
+num = int(input())
+count = num // 4
+count_str =  "long "
+
+if num % 4 == 0:
+    print(count_str * count + "int")
+
+elif num % 4 != 0:
+    print(count_str * (count + 1) + "int")
