aa75fa419a99c4c5d40163c3cbf827403c87b342
aszxk012 <aszxk012@naver.com>
2023-04-21 00:13:45 +0900

    24days

--- basics/2675.py
+++ basics/2675.py
@@ -1,10 +1,11 @@
 # 2675
 # 문자열 s를 입력 받은 후 첫번째 문자를 r번, 두번째 문자를 r번 반복하는 식으로 새 문자열 p를 만든다.
 # 문자 단위로 줄바꿈해서 출력됨 > 추후 수정(2023.04.02)
+# 수정 완료(2023.04.21)
 
 test = int(input())
-nums = []
-string = []
+result = []
+string = ''
 
 
 for _ in range(test):
@@ -14,7 +15,10 @@ for _ in range(test):
     num = int(num)
 
     for i in range(len(char)):
-        string.append(char[i] * num)
+        string += char[i] * num
+        
+    result.append(string)
+    string = ''
 
-for i in range(len(string)):
-    print(string[i])
+for i in range(len(result)):
+    print(result[i])
