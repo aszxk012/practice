504562022e39bd0a49d5d2379f1832e226b717c6
aszxk012 <aszxk012@naver.com>
2023-04-23 19:11:46 +0900

    26days

--- basics/5355.py
+++ basics/5355.py
@@ -3,33 +3,31 @@
 # 테스트 케이스 개수가 주어지고 화성수학식이 한 줄에 하나씩 주어지며 소수점 둘째자리까지 출력
 # 못 풀었음 > 추후 수정(2023.04.02)
 
-import sys
-
 test = int(input())
 temp_num = 0
 result = []
 
 for _ in range(test):
-    # 개행문자 > \n 같은 문자 제거 함수 rstrip()
-    # char = sys.stdin.readline().rstrip()
-    # cal = char.split(' ')
-
     char = input()
-    num, oper = char.split(' ')
-    
-    num = int(num)
+    char_split = char.split(' ')
 
-    if "@" in oper:
-        temp_num = num * 3
+    temp_num = float(char_split[0])
+    del char_split[0]
 
-    elif "#" in oper:
-        temp_num = num - 7
+    oper = char_split
     
-    elif "%" in oper:
-        temp_num = num + 5
+    for i in oper:
+        if i == "@":
+            temp_num = temp_num * 3
+
+        elif i == "#":
+            temp_num = temp_num - 7
+    
+        elif i == "%":
+            temp_num = temp_num + 5
 
     result.append(temp_num)
 
 
 for i in range(test):
-    print(result[i])
+    print("%.2f"%result[i])
