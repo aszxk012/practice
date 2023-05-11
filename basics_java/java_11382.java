aa2376f0e8f3f3b69a15e66977bf42f115046784
aszxk012 <aszxk012@naver.com>
2023-05-11 22:48:20 +0900

    43days

---
+++ basics_java/java_11382.java
@@ -0,0 +1,16 @@
+/* 11382
+ * A + B + C 계산하기
+ */
+
+import java.util.*;
+public class java_11382 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
---
+++ basics_java/java_11718.java
@@ -0,0 +1,22 @@
+/* 11718
+ * 입력받은 그대로 출력하기
+ * EOF
+ */
+
+import java.util.*;
+
+public class java_11718 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        List<String> result = new ArrayList<String>();
+
+        while (scanner.hasNext()) {
+            result.add(scanner.nextLine());
+        }
+
+        int i;
+        for(i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
+    }
+}
