32e61d0e353d2be5d0e685c9e463e94401520d7d
aszxk012 <aszxk012@naver.com>
2023-07-16 01:59:51 +0900

    107days

--- basics_java/java_2822.java
+++ basics_java/java_2822.java
@@ -2,42 +2,33 @@
  * 8줄에 걸쳐서 문제에 대한 참가자의 점수가 주어진다.
  * 참가자의 총 점수는 가장 높은 점수 5개의 합이다.
  * 참가자의 총 점수와 어떤 문제가 최종 점수에 포함되는지 출력하기
- * 추후 수정(2023.07.09)
+ * 추후 수정(2023.07.09) > index 출력 추후 수정(2023.07.16)
  */
 
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 public class java_2822 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int scores[] = new int[8];
-        ArrayList<Integer> max_scores = new ArrayList<Integer>();
+        int index[] = new int[8];
+        int max_scores[] = new int[5];
         int sum = 0;
 
         for (int i = 0; i < 8; i++) {
             scores[i] = scanner.nextInt();
+            index[i] = i + 1;
         }
 
-        for (int score : scores) {
-            if (max_scores.size() < 5) {
-                max_scores.add(score);
-                sum += score;
-            }
-            else {
-                for (int i = 0; i < 5; i++) {
-                    if (score > max_scores.get(i)) {
-                        sum -= max_scores.get(i);
-                        max_scores.remove(Integer.valueOf(max_scores.get(i)));
-                        max_scores.add(score);
-                        sum += score;
-                    } 
-                }
-            }
+        Arrays.sort(scores);
+        
+        for (int i = 0; i < 5; i++) {
+            max_scores[i] = scores[7 - i];
+            sum += max_scores[i];
         }
 
         System.out.println(sum);
-        for (int score : max_scores) {
-            System.out.print(score + " ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(index[i] + " ");
         }
         scanner.close();
     }
