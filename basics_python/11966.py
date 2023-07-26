106d76422c586b172be7812c1d312be987a59dbb
aszxk012 <aszxk012@naver.com>
2023-07-26 23:31:32 +0900

    116days

---
+++ basics_python/11966.py
@@ -0,0 +1,19 @@
+# 11966
+# 자연수 n이 2의 제곱이면 1, 아니면 0 출력하기
+# log 말고 비트 연산으로 다시 풀어보기(2023.07.26)
+import math
+
+n = int(input())
+
+if n != 1:
+    if (n % 2 != 0):
+        print(0)
+    else:
+        a = math.log(n, 2)
+        if not a - int(a):
+            print(1)
+        else:
+            print(0)
+
+else:
+    print(1)
