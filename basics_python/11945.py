1b07a17bd0a82983ba3d2cb10ef2be4f1dbc8e6f
aszxk012 <aszxk012@naver.com>
2023-08-01 22:06:10 +0900

    122days

--- basics_python/11945.py
+++ basics_python/11945.py
@@ -1,17 +1,19 @@
 # 11945
-# 오류 미수정(2023.07.27)
+# 오류 미수정(2023.07.27) > 오류 수정(2023.08.01)
 
 n, m = map(int, input().split(' '))
 fish = []
 result = []
 
 for i in range(n):
-    cur = input()
-    fish.append(cur)
-
-    for j in range(m):
-        string = fish[i]
-        fish[i] = string[::-1]
-
-for i in range(n - 1, -1, -1):
-    print(fish[i])
+    fish.append(input())
+    
+for i in range(n):
+    for j in range(m - 1, -1, -1):
+        result.append(fish[i][j])
+        
+for i in range(n * m):
+    print(result[i], end='')
+    
+    if ((i + 1) % m == 0):
+        print()
