a27b98620a253a2fa07a13fb713cbb2f6b77c86b
aszxk012 <aszxk012@naver.com>
2023-08-14 23:36:15 +0900

    134days

--- basics_python/1929.py
+++ basics_python/1929.py
@@ -1,6 +1,6 @@
 # 1929
 # m 이상 n 이하의 소수 모두 출력
-# 시간초과 추후 수정(2023.08.11)
+# 시간초과 추후 수정(2023.08.11) > 
 
 m, n = map(int, input().split(' '))
 result = []
@@ -15,7 +15,23 @@ for i in range(m, n + 1):
     factors = list(set(factors))
     
     if(len(factors) == 2):
-        result.append(i)
-            
-for num in result:
-    print(num)
+        print(i)
+
+
+
+# ==========================================================
+# 에라토스테네스의 체 사용하여 풀기
+# 런타임 에러 추후 수정(2023.08.14)
+
+m, n = map(int, input().split(' '))
+prime_list = [True] * [n + 1]
+prime_list[0], prime_list[1] = False, False
+
+for i in range(2, int(n ** 0.5) + 1):
+    if prime_list[i]:
+        for j in range(i * 2, n + 2, i):
+            prime_list[j] = False
+
+for i in range(m, n + 1):
+    if prime_list[i]:
+        print(i)
