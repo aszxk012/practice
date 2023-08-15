cfc9084720769da29b659da2338bdddb2d5627a4
aszxk012 <aszxk012@naver.com>
2023-08-15 23:26:13 +0900

    135days

--- basics_python/1929.py
+++ basics_python/1929.py
@@ -1,6 +1,6 @@
 # 1929
 # m 이상 n 이하의 소수 모두 출력
-# 시간초과 추후 수정(2023.08.11) > 
+# 시간초과 추후 수정(2023.08.11)
 
 m, n = map(int, input().split(' '))
 result = []
@@ -21,15 +21,15 @@ for i in range(m, n + 1):
 
 # ==========================================================
 # 에라토스테네스의 체 사용하여 풀기
-# 런타임 에러 추후 수정(2023.08.14)
+# 런타임 에러 추후 수정(2023.08.14) > 오류 수정(2023.08.15)
 
 m, n = map(int, input().split(' '))
-prime_list = [True] * [n + 1]
+prime_list = [True] * (n + 1)
 prime_list[0], prime_list[1] = False, False
 
 for i in range(2, int(n ** 0.5) + 1):
     if prime_list[i]:
-        for j in range(i * 2, n + 2, i):
+        for j in range(i * 2, n + 1, i):
             prime_list[j] = False
 
 for i in range(m, n + 1):
