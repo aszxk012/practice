d300f9c488257a105d30d312eb998de66fbbe4cd
aszxk012 <aszxk012@naver.com>
2023-10-09 22:31:22 +0900

    189days

--- basics_java/java_29863.java
+++ basics_java/java_29863.java
@@ -1,5 +1,8 @@
 /* 29863
- * 어디가 틀렸는지 모르겠음(추후 수정 23.10.03)
+ * 첫 번째 줄: 20 ~ 23, 0 ~ 3 : 잠드는 시간
+ * 두 번째 줄: 5 ~ 10, 깨어나는 시간
+ * 잠 자는 시간 출력하기
+ * 어디가 틀렸는지 모르겠음(추후 수정 23.10.03) > 수정 완료(23.10.09)
  */
 import java.util.*;
 public class java_29863 {
@@ -10,6 +13,9 @@ public class java_29863 {
 
         int hour = (24 - sleep) + wake;
 
+        if (hour >= 24) {
+            hour -= 24;
+        }
         System.out.println(hour);
         
         scanner.close();
