a6bb14617a7ecc5313977ab2db00c15a7f4f00da
aszxk012 <aszxk012@naver.com>
2023-11-12 23:46:39 +0900

    221days

--- basics_python/14501.py
+++ basics_python/14501.py
@@ -1,22 +1,28 @@
 # 14501
 # 퇴사하는 날 N, Ti(상담하는 데 걸리는 시간)와 Pi(가격)가 공백으로 구분되어 주어질 때
 # 퇴사하기 전까지 최대로 얻을 수 있는 수익 출력하기
-# 문제 이해 필요(2023.11.10) >> 답 불일치(2023.11.11)
+# 문제 이해 필요(2023.11.10) >> 답 불일치(2023.11.11, 12)
 
 n = int(input())
 salary = []
-count = n; price = 0
+count = n - 1; price = 0
 
 for i in range(n):
     temp_list = list(map(int, input().split(' ')))
     salary.append(temp_list)
 
 for i in range(n - 1, -1, -1):
-    #print(salary[i][0])
+    if (count == i):
+        if (i + salary[i][0] <= n):
+            print(salary[i])
 
-    if (count + salary[i][0] <= n + 1):
-        print(salary[i])
-        price += salary[i][1]
+            price += salary[i][1]
+            count -= salary[i][0]
+
+            print(i)
+    else:
+        pass
 
     count -= 1
+
 print(price)
