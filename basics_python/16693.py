097c440a80fa4f8d084bca1cc0417564cc7237d4
aszxk012 <aszxk012@naver.com>
2023-12-15 23:36:02 +0900

    254days

--- basics_python/16693.py
+++ basics_python/16693.py
@@ -1,23 +1,17 @@
 # 16693
 # 조각피자 A1 넓이의 가격 P1, 원형 피자 R1 반지름에 가격 P2
 # 어느 피자를 구매하는 것이 나은지 출력하기
-# 오답 추후 수정(23.12.13, 14)
+# 오답 추후 수정(23.12.13, 14, 15)
 
 a, p1 = map(int, input().split(' '))
 r, p2 = map(int, input().split(' '))
 
 b = 3.14 * r * r
-ratio = a / b
-b_p = p2 * ratio
+a_ratio = a / p1
+b_ratio = b / p2
 
-if (a > b):
-    if (p1 <= p2):
-        print("Slice of pizza")
-    else:
-        print("Whole pizza")
+if (a_ratio <= b_ratio):
+    print("Whole pizza")
 
 else:
-    if (p1 >= p2):
-        print("Whole pizza")
-    else:
-        print("Slice of pizza")
+    print("Slice of pizza")
