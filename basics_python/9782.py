285bb22596640773817399c15bc9185553299d12
aszxk012 <aszxk012@naver.com>
2024-03-11 23:49:09 +0900

    336days

--- basics_python/9782.py
+++ basics_python/9782.py
@@ -3,25 +3,21 @@
 # n개의 데이터가 주어졌을 때, n이 홀수인 경우 중앙값은 (n + 1) / 2번째에 위치하고 있으며
 # n이 짝수인 경우 n / 2번째 데이터와 (n / 2) + 1번째 데이터의 평균이다.
 # 중앙값을 출력
-# 런타임 오류(2024.01.20, 21, 02.13)
+# 런타임 오류(2024.01.20, 21, 02.13, 03.11)
 
 i = 1
 while True:
     num_list = list(map(int, input().split(' ')))
-    n = num_list[0]
-    if (n == 0):
+    if (num_list[0] == 0):
         break
     
-    num_list = num_list[1:]
-    num_list.sort()
-
+    n, num_list = num_list[0], num_list[1:]
+    
     if (n % 2 == 0):
-        temp = n // 2 - 1
-        median = (num_list[temp] + num_list[temp + 1]) // 2
+        median = (num_list[n // 2 - 1] + num_list[n // 2]) / 2
 
     elif (n % 2 != 0):
-        temp = (n - 1) // 2
-        median = num_list[temp]
+        median = num_list[(n + 1) // 2 - 1]
 
     print("Case %d: %.1f" % (i, median))
     i += 1
