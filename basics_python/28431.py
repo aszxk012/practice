b6792a3e29f07611d49c630ef6b40ac47c99793d
aszxk012 <aszxk012@naver.com>
2024-04-18 21:48:16 +0900

    374days

--- basics_python/28431.py
+++ basics_python/28431.py
@@ -2,22 +2,21 @@
 # 숫자가 쓰여진 양말 5개가 주어진다.
 # 같은 숫자가 쓰여진 양말 두 개를 모으면 양말 한 쌍이 된다.
 # 쌍을 만들 수 있는 양말을 뺀 후 남은 양말에 쓰인 숫자 출력
-# 같은 숫자가 3개인 경우 미해결(2024.02.09, 10)
+# 같은 숫자가 3개인 경우 미해결(2024.02.09, 10) > 해결(2024.04.18.)
+
 socks = []
-result = []
 
 for _ in range(5):
     cur = int(input())
     socks.append(cur)
 
-
-for i in range(len(socks)):
-    for j in range(i + 1, len(socks)):
-        if (socks[i] == socks[j]):
-            result.append(socks[i])
+for i in range(5):
+    for j in range(i + 1, 5):
+        if socks[i] == socks[j]:
+            socks[i] = socks[j] = None
             break
 
-for i in range(len(socks)):
-    if (not socks[i] in result):
-        print(socks[i])
+for sock in socks:
+    if sock is not None:
+        print(sock)
         break
