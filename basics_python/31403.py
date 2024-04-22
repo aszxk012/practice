01f8921fea46fab05337f3b863ab9e8986822fee
aszxk012 <aszxk012@naver.com>
2024-04-22 22:58:15 +0900

    378days

---
+++ basics_python/31403.py
@@ -0,0 +1,17 @@
+# 31403
+# A, B, C가 각각 주어질 때 수와 문자열로 생각할 경우 A + B - C 출력
+# 에러 발생(2024.04.22.)
+
+a = input()
+b = input()
+c = input()
+
+an = int(a)
+bn = int(b)
+cn = int(c)
+
+total_n = an + bn  - cn
+total = a + b - c
+
+print(total_n)
+print(total)
