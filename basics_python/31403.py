8efcc4eb758772f5616399379a431062e01bbe53
aszxk012 <aszxk012@naver.com>
2024-04-23 21:05:36 +0900

    379days

--- basics_python/31403.py
+++ basics_python/31403.py
@@ -1,6 +1,6 @@
 # 31403
 # A, B, C가 각각 주어질 때 수와 문자열로 생각할 경우 A + B - C 출력
-# 에러 발생(2024.04.22.)
+# 에러 발생(2024.04.22.) > 문제 해결(2024.04.23.)
 
 a = input()
 b = input()
@@ -11,7 +11,9 @@ bn = int(b)
 cn = int(c)
 
 total_n = an + bn  - cn
-total = a + b - c
+total = a + b
+total, c = int(total), int(c)
+total -= c
 
 print(total_n)
 print(total)
