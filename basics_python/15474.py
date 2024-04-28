4202258a81c8f50448c9ed42ebc45985c76a1683
aszxk012 <aszxk012@naver.com>
2024-04-28 21:32:37 +0900

    384days

--- basics_python/15474.py
+++ basics_python/15474.py
@@ -1,52 +1,17 @@
 # 15474
 # 세트 X는 A개에 B엔, 세트 Y는 C개에 D엔일 때
 # 연필 N개를 구매할 수 있는 최소 금액 출력하기
-# 시간초과 추후 수정(23.12.11, 24.02.11)
+# 시간초과 추후 수정(23.12.11, 24.02.11) > 해결(2024.04.28.)
 
 n, a, b, c, d = map(int, input().split(' '))
-price_a = 0
-price_b = 0
-count_a = 0
-count_b = 0
+X_set = n // a
+Y_set = n // c
 
-while True:
-    if (count_a == n):
-        if (count_a == count_b):
-            if (price_a >= price_b):
-                print(price_b)
-            else:
-                print(price_a)
+if (n % a != 0):
+    X_set += 1
 
-        else:
-            print(price_a)
+if (n % c != 0):
+    Y_set += 1
 
-        break
-
-    elif (count_b == n):
-        if (count_a == count_b):
-            if (price_a >= price_b):
-                print(price_b)
-            else:
-                print(price_a)
-        
-        else:
-            print(price_b)
-        break
-
-    else:
-        price_a += b
-        price_b += d
-
-        count_a += a
-        count_b += c
-
-# ---------------------------------------------------
-
-N, A, B, C, D = map(int, input().split())
-
-Y_sets = N // C
-remaining_pencils = N - Y_sets * C
-
-total_price = min(Y_sets * D + remaining_pencils * B, (N // A + (1 if N % A != 0 else 0)) * B)
-
-print(total_price)
+final = min(X_set * b, Y_set * d)
+print(final)
